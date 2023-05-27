4cccc764347249fc03f58c44872f4f1939a8fc7d
mayankch0786 <132358628+mayankch0786@users.noreply.github.com>
2023-05-27 22:41:48 +0530

    method m1() added by devloper 2

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -5,4 +5,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.print("This is demo classes added for GIT POC");
     }
+    
+    public void m1(){
+     System.out.println("method added by devloper 2");   
+    }
 }
